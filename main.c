dd4aa3af73fd5042aadfa10e0e9e7f681ba5c302
Kaleb Teshale <kalebteshale72@gmail.com>
2023-08-23 11:51:34 +0300

    first task

---
+++ main.c
@@ -0,0 +1,16 @@
+#include "shell.h"
+
+/**
+ * main - the main shell code
+ * @argc: number of arguments passed
+ * @argv: program arguments to be parsed
+ *
+ * Return: 0 on success
+ */
+
+int main(int ac, char **argv)
+{
+    printf("passed the betty checker");
+
+    return 0;
+}
